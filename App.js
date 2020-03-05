48dea4355117559ebc363629a30bc83c7011d98b
Rafael Santana <raf.santa11@gmail.com>
2020-03-05 14:01:29 -0300

    implementando adicionar objetivos

---
+++ App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={{ padding: 50 }}>
+      <View style={{ flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center' }}>
+        <TextInput placeholder="Objetivos"
+          style={{ borderColor: 'black', borderWidth: 1, padding: 10, width: '80%' }}
+        />
+        <Button title="ADD" />
+      </View>
+      <View>
+
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+
+});
